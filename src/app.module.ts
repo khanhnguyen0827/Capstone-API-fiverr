3abc2df4fe0229360edf6161e8b4412b15dc8fec
Nguyễn Quốc Khánh <68071460+khanhnguyen0827@users.noreply.github.com>
2025-08-18 19:59:34 +0700

    Add initial Auth module and controller
    Introduces the AuthModule and an empty AuthController. Also registers AuthModule in the main AppModule to prepare for authentication feature development.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [AppController],
-
-
-
   providers: [AppService],
 })
 export class AppModule {}
