93b8694cb0f14c7e6945ab593711f574c799f4a5
Nguyễn Quốc Khánh <68071460+khanhnguyen0827@users.noreply.github.com>
2025-08-18 20:37:25 +0700

    Integrate Prisma ORM and scaffold Auth module
    Added Prisma ORM setup with schema, service, and dependencies. Scaffolded the Auth module with controller and service, and registered PrismaService for database access. Updated .gitignore for generated Prisma client output.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
+@Controller("auth")
 
 export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Get("list")
+    findAll() {
+        const data = this.authService.findAll();
+        return {
+            message: data,
+        };
+    }
+  
 }
